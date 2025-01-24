24682038daec4b17290f8c1e1ad27b0e89352400
Yuan Chen <cysbc1999@gmail.com>
2025-01-23 23:29:45 -0500

    add frontend backend connection

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -22,8 +22,8 @@ export function Navbar({
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex items-center">
-              <Link to="/" className="text-2xl font-bold">
-                <span className="text-[#861F41]">Offers+</span>
+              <Link to="/" className="text-2xl font">
+                <span className="text-[#861F41]">OfferPlus</span>
               </Link>
             </div>
             
